b4c3131826ac98ffac6118e5ba87d2e156c6b78b
yxs123321-jpg <yxs123321@gmail.com>
2026-09-01 23:15:59 +0800

    Update update.sh
    Signed-off-by: yxs123321-jpg <yxs123321@gmail.com>

--- wrt_core/update.sh
+++ wrt_core/update.sh
@@ -100,8 +100,6 @@ stage_pre_install_source_fixes() {
     add_quickfile
     add_daede
     add_minigate
-    add_dae
-    add_v2ray_geodata
     update_lucky
     fix_rust_compile_error
     update_smartdns
