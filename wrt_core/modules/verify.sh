406be3740779b361b81a70368440bf549e32ec4f
yxs123321-jpg <yxs123321@gmail.com>
2026-09-09 09:12:15 +0800

    Update verify.sh
    Signed-off-by: yxs123321-jpg <yxs123321@gmail.com>

--- wrt_core/modules/verify.sh
+++ wrt_core/modules/verify.sh
@@ -6,7 +6,7 @@ verify_custom_feed_installed_paths() {
     # install_feeds 后必须存在的 custom_feed 包路径。
     local required_package_dirs=(
         luci-app-adguardhome luci-app-easytier
-        luci-app-passwall nikki luci-app-nikki mihomo-meta luci-app-emmc-health
+        nikki luci-app-nikki mihomo-meta luci-app-emmc-health
     )
     local missing_package_dirs=()
     custom_feed_name=$(get_custom_feed_name)
